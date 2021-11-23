724bf131cde77fc3b5378fb0b303f017a4f89dae
Yashdeep patwa <57016712+yashpatwa60@users.noreply.github.com>
2021-11-24 01:30:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Home from './pages/landingPage'
 import Login from './pages/login'
-import { HashRouter as Router, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/" exact element={ <Home/> } />
           <Route path="/login" exact element={ <Login/> } />
         </Routes>
-      </Router> 
     </div>
   );
 }
